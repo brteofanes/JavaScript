33a59c48e9ebb369d3d710af7d5a363f58dca747
Teófanes <brunoteofanes@gmail.com>
2025-04-23 22:16:04 -0300

    Continuação.
    Biblioteca  numérica.

---
+++ javascript10.js
@@ -0,0 +1,13 @@
+
+
+//Math.round(9.8);  Arredonda para o inteiro mais próximo.
+
+//Math.floor(9.8);    Arredonda para baixo.
+
+//Math.ceil(9.8);     Arredonda para cima memos que a parte decimal seja mínima.
+
+//Math.trunc(9.8)     Corta a parte decimal.
+
+
+
+
